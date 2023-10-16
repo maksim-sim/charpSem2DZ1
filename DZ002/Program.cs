8d51ce63cbc48e188dd3fa6477eb3c4880c521a0
maksim kireev <maksimkir987@gmail.com>
2023-10-16 22:13:25 +0300

    first2 commit

---
+++ DZ002/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int input = Convert.ToInt32(Console.ReadLine());
+
+int thirdDigit = GetThirdDigit(input);
+
+if (thirdDigit != -1) Console.WriteLine("Третья цифра числа: " + thirdDigit);
+else Console.WriteLine("Ошибка! Введено некорректное число или третьей цифры нет.");
+
+static int GetThirdDigit(int input){
+    if (input > 99){
+        while (input > 999) input /= 10;
+        return input % 10;
+    }
+    return -1;
+}
