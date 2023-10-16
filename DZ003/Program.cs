2de0d8a036461e4c614e8004612eb0a09dd1bcb6
maksim kireev <maksimkir987@gmail.com>
2023-10-16 22:13:46 +0300

    first3 commit

---
+++ DZ003/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число день недели:");
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int number) && number > 0 && number < 8){
+    if (number > 5) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
+}else Console.WriteLine("Ошибка! Введено некорректное число дня недели.");
