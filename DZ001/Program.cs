69190d01344675bd697369c1a88db9aa054f3dc6
maksim kireev <maksimkir987@gmail.com>
2023-10-16 22:12:05 +0300

    first1 commit

---
+++ DZ001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+string input = Console.ReadLine();
+
+int secondDigit = GetSecondDigit(input);
+
+if (secondDigit != -1) Console.WriteLine("Вторая цифра числа: " + secondDigit);
+else Console.WriteLine("Ошибка! Введено некорректное трехзначное число.");
+
+static int GetSecondDigit(string input){
+        if (input.Length == 3 && int.TryParse(input, out int number)) return (number / 10) % 10;
+        else return -1;
+}
